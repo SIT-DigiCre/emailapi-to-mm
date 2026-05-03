61c4196125587437ea5c89c746426dcdcdf4c518
tsukumo <contact@tsukumo.f5.si>
2026-05-04 05:35:33 +0900

    一応動くはず？

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ struct SendGridEvent {
 
 #[derive(Serialize)]
 struct MmEvent {
+    username: String,
     text: String,
 }
 
@@ -24,6 +25,38 @@ async fn fetch(req: Request, env: Env, _ctx: Context) -> Result<Response> {
         return Response::error("", 401);
     }
     // 認証 :done:
+
+    let mut req = req;
+    let req_data: Vec<SendGridEvent> = if let Ok(res) = req.json().await {
+        res
+    } else {
+        return Response::error("", 400);
+    };
+
+    let mm_webhook_url = env.secret("MATTERMOST_WEBHOOK_URL")?.to_string();
+    let username = env
+        .var("MATTERMOST_USERNAME")
+        .map_or_else(|_| "メール送信お知らせくん".to_string(), |v| v.to_string());
+
+    for ev in req_data.iter().filter(|ev| ev.event == "Processed") {
+        let mm_ev = MmEvent {
+            text: format!("`{}`へのメール配信処理が実行されました。", ev.email),
+
+            username: username.clone(),
+        };
+        let body = Some(serde_json::to_string(&mm_ev)?.into());
+
+        let mm_init = RequestInit {
+            method: Method::Post,
+            headers: Headers::from_iter([("Content-Type", "application/json")]),
+            body,
+            ..Default::default()
+        };
+
+        Fetch::Request(Request::new_with_init(&mm_webhook_url, &mm_init)?)
+            .send()
+            .await?;
+    }
 
     Response::ok("")
 }
